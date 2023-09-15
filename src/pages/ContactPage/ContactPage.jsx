619139288af6f40a04c8b1f59c791d4783135a8a
eukeo <sixsblooms@gmail.com>
2023-09-15 01:07:11 -0700

    form added

--- src/pages/ContactPage/ContactPage.jsx
+++ src/pages/ContactPage/ContactPage.jsx
@@ -3,8 +3,8 @@ import "./ContactPage.css";
 const ContactPage = () => {
   return (
     <div className="contactContainer">
-      <section className="contactInfo">
-        <h4 id="info">Contact Information</h4>
+      {/* <section className="contactInfo">
+        <h4 id="info">Contact</h4>
         <div className="links">
           <a href="https://www.linkedin.com/in/gabrielgomez-" target="_blank">
             <img src="https://cdn.icon-icons.com/icons2/2428/PNG/512/linkedin_black_logo_icon_147114.png"></img>
@@ -16,7 +16,6 @@ const ContactPage = () => {
             <img src="https://cdn-icons-png.flaticon.com/512/25/25231.png"></img>
           </a>
         </div>
-        <h4>Email: gabegomez419@gmail.com</h4>
         <a
           href="https://docs.google.com/document/d/e/2PACX-1vSG_u6o5FJqckN50az6h5NaaLIMKjy4y3JtolpViCDd7-WhNU0SJb_PZ6izUPxsoDIpr7C5DWty9Uto/pub"
           download="Gabriel-Gomez-Resume"
@@ -25,7 +24,24 @@ const ContactPage = () => {
         >
           Resume
         </a>
-      </section>
+      </section> */}
+      <form>
+        <h3>Get In Touch</h3>
+        <input type="text" id="name" placeholder="Your Name" required></input>
+        <input type="email" id="email" placeholder="Email" required></input>
+        <input
+          type="text"
+          id="phone"
+          placeholder="Phone Number"
+          required
+        ></input>
+        <textarea
+          id="message"
+          rows="5"
+          placeholder="How can I help you?"
+        ></textarea>
+        <button type="submit">Send</button>
+      </form>
     </div>
   );
 };
