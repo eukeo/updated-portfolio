ac8bfa7dfb49862fd6aa6780d5717bae6e2e0f19
eukeo <sixsblooms@gmail.com>
2023-09-18 18:25:28 -0700

    home page changes

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -9,6 +9,14 @@ const HomePage = () => {
         className="profilePicture"
       ></img>
       <h5>Front-End Developer</h5>
+      <a
+        href="https://docs.google.com/document/d/e/2PACX-1vSG_u6o5FJqckN50az6h5NaaLIMKjy4y3JtolpViCDd7-WhNU0SJb_PZ6izUPxsoDIpr7C5DWty9Uto/pub"
+        download="Gabriel-Gomez-Resume"
+        target="_blank"
+        className="resumeLink"
+      >
+        Resume
+      </a>
     </div>
   );
 };
