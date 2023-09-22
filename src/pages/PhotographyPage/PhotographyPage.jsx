ecd6ddf2afade46c4b067c36e31aa65e2014a52b
eukeo <sixsblooms@gmail.com>
2023-09-22 12:12:41 -0700

    photography-added

--- src/pages/PhotographyPage/PhotographyPage.jsx
+++ src/pages/PhotographyPage/PhotographyPage.jsx
@@ -2,8 +2,19 @@ import "./PhotographyPage.css";
 
 const PhotographyPage = () => {
   return (
-    <div>
-      <h1>Photography</h1>
+    <div className="photography">
+      <img src="https://i.imgur.com/Lchu5xL.jpeg"></img>
+      <img src="https://i.imgur.com/22BzVpw.jpeg"></img>
+      <img src="https://i.imgur.com/n7wxAXL.jpeg"></img>
+      <img src="https://i.imgur.com/KFLy88o.jpeg"></img>
+      <img src="https://i.imgur.com/vvsrycT.jpeg"></img>
+      <img src="https://i.imgur.com/1infr3S.jpeg"></img>
+      <img src="https://i.imgur.com/V01P9lR.jpeg"></img>
+      <img src="https://i.imgur.com/xh8cZ0L.jpeg"></img>
+      <img src="https://i.imgur.com/cMAuoRX.jpeg"></img>
+      <img src="https://i.imgur.com/RCuh3FM.jpeg"></img>
+      <img src="https://i.imgur.com/pRWTFpW.jpeg"></img>
+      <img src="https://i.imgur.com/RLq9qW6.jpeg"></img>
     </div>
   );
 };
