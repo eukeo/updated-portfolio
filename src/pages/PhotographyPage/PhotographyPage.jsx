7da5bf8f0552ff472ea57fbe8b4e1e9c7a8ead1c
eukeo <sixsblooms@gmail.com>
2023-09-25 22:58:45 -0700

    photogaphy-pic-edits

--- src/pages/PhotographyPage/PhotographyPage.jsx
+++ src/pages/PhotographyPage/PhotographyPage.jsx
@@ -10,17 +10,14 @@ const PhotographyPage = () => {
 
   return (
     <div className="photography">
-      <img data-aos="zoom-in" src="https://i.imgur.com/Lchu5xL.jpeg"></img>
-      <img data-aos="zoom-in" src="https://i.imgur.com/22BzVpw.jpeg"></img>
-      <img data-aos="zoom-in" src="https://i.imgur.com/n7wxAXL.jpeg"></img>
-      <img data-aos="zoom-in" src="https://i.imgur.com/KFLy88o.jpeg"></img>
-      <img data-aos="zoom-in" src="https://i.imgur.com/vvsrycT.jpeg"></img>
       <img data-aos="zoom-in" src="https://i.imgur.com/1infr3S.jpeg"></img>
       <img data-aos="zoom-in" src="https://i.imgur.com/V01P9lR.jpeg"></img>
+      <img data-aos="zoom-in" src="https://i.imgur.com/RCuh3FM.jpeg"></img>
+      <img data-aos="zoom-in" src="https://i.imgur.com/Lchu5xL.jpeg"></img>
+      <img data-aos="zoom-in" src="https://i.imgur.com/pRWTFpW.jpeg"></img>
       <img data-aos="zoom-in" src="https://i.imgur.com/xh8cZ0L.jpeg"></img>
       <img data-aos="zoom-in" src="https://i.imgur.com/cMAuoRX.jpeg"></img>
-      <img data-aos="zoom-in" src="https://i.imgur.com/RCuh3FM.jpeg"></img>
-      <img data-aos="zoom-in" src="https://i.imgur.com/pRWTFpW.jpeg"></img>
+      <img data-aos="zoom-in" src="https://i.imgur.com/KFLy88o.jpeg"></img>
       <img data-aos="zoom-in" src="https://i.imgur.com/RLq9qW6.jpeg"></img>
     </div>
   );
