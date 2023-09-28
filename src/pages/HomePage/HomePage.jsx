6dc06111dce083e0194ad1c6b7889a8f56fe6ae9
eukeo <sixsblooms@gmail.com>
2023-09-28 16:45:58 -0700

    home-page-updates

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -10,26 +10,7 @@ const HomePage = () => {
 
   return (
     <div className="homeContainer">
-      <div className="myName">
-        <h1 className="first" data-aos="fade-right">
-          Gabriel
-        </h1>
-        <h1 className="last" data-aos="fade-left">
-          Gomez
-        </h1>
-      </div>
-      {/* <img
-        src="https://i.imgur.com/p4USbTp.jpeg"
-        className="profilePicture"
-      ></img> */}
-      {/* <a
-        href="https://docs.google.com/document/d/e/2PACX-1vSG_u6o5FJqckN50az6h5NaaLIMKjy4y3JtolpViCDd7-WhNU0SJb_PZ6izUPxsoDIpr7C5DWty9Uto/pub"
-        download="Gabriel-Gomez-Resume"
-        target="_blank"
-        className="resumeLink"
-      >
-        Resume
-      </a> */}
+      <img src="https://i.imgur.com/6C8V6aB.png" className="myName"></img>
     </div>
   );
 };
